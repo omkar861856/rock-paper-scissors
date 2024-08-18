c6ec6b2207d10374d038c1fa401b0ef94462bd34
omkar861856 <omkar861856@gmail.com>
2024-08-18 15:50:33 +0530

    result screen made

--- script.js
+++ script.js
@@ -1,68 +1,100 @@
-let wins = 0;
-// let losses = 0;
-// let draws = 0;
+const props = ["rock", "paper", "scissors"];
 
-document.getElementById("wins").innerText = `${wins}`;
+const wins = document.getElementById('wins');
+wins.innerText = window.localStorage.getItem('wins')
 
-function WinLogic(player, computer) {
-
-  // if (player === computer) {
-  //   draws += 1;
-  //   document.getElementById("result").innerText = "draw"
-  //   document.getElementById("draws").innerText = "Draws: " + draws;
-
-  // }
-  if ((player === "rock" && computer === "scissors") || (player === "scissors" && computer === "paper") || (player === "paper" && computer === "rock")) {
-    wins += 1;
-    // document.getElementById("result").innerText = "win"
-    document.getElementById("wins").innerText = `${wins}`;
-
-  }
-  // else {
-  //   losses += 1;
-  //   document.getElementById("result").innerText = "loose"
-  //   document.getElementById("losses").innerText = "Losses: " + losses;
-
-  // }
-
-
-}
-
-
-
-
-function Computer(...args) {
-  const values = args;
-  const random = Math.floor(Math.random() * values.length);
-  return values[random];
-}
 
 
 function ComputerTurn(player) {
 
-  const computer = Computer("rock", "paper", "scissors");
 
-  const compTurn = document.getElementsByClassName(computer);
+    // Randomise computer turn
+  const computer = Math.floor(Math.random() * props.length);
+  const computerTurn = props[computer];
 
-  const playerTurn = document.getElementById(player)
+  // get the result 
 
-  // console.log(compTurn[0])
+  const result = ResultLogic(player, computerTurn);
 
-  if (compTurn.length > 0) {
+  // set wins to local storage
+  wins.innerText = window.localStorage.getItem('wins')
 
-    const computerDisplay = document.getElementById('computer_display');
-    // Clear previous content if needed
-    computerDisplay.innerHTML = '';
+  
+  // player button
+  const playerButton = document.getElementById(player).cloneNode(true)
+  playerButton.classList.remove(player)
+  playerButton.classList.add('player')
+  playerButton.setAttribute('disabled', true);
 
-    // Append the element to the computer_display element
-    computerDisplay.appendChild(compTurn[0].cloneNode(true));
 
+//   result and reset button
+  const resetButton = document.createElement('button')
+  resetButton.textContent = 'Restart'
+  resetButton.addEventListener('click', () => {
+    window.location.reload();
+  })
+  const declarationDiv = document.createElement('div');
+  declarationDiv.textContent = `${result.result}`;
+  declarationDiv.appendChild(resetButton)
+  declarationDiv.id = 'declaration'
+
+
+//   computor button
+  let computerButton = document.getElementById(computerTurn).cloneNode(true)
+  computerButton.classList.remove(computerTurn)
+  computerButton.classList.add('computer')
+  computerButton.setAttribute('disabled', true);
+
+// clear the play area and display result
+  const area = document.getElementById('area');
+  area.id = 'result'
+  area.innerHTML = ""
+  area.appendChild(playerButton)
+  area.appendChild(declarationDiv)
+  area.appendChild(computerButton)
+}
+
+// result logic
+function ResultLogic(player, computer) {
+
+    let wins= parseInt(window.localStorage.getItem('wins'));
+    let draws=parseInt(window.localStorage.getItem('draws'));
+    let losses= parseInt(window.localStorage.getItem('losses'));
+
+    console.log(wins, draws, losses)
+
+    if (player === computer) {
+      draws += 1;    
+      window.localStorage.setItem('draws', draws)
+      return {draws, result: "It's a draw"}
+  
+    }
+    if ((player === "rock" && computer === "scissors") || (player === "scissors" && computer === "paper") || (player === "paper" && computer === "rock")) {
+
+      wins += 1;
+      window.localStorage.setItem('wins', wins)
+
+      return {wins, result: "You win"};
+  
+    }
+    else {
+      losses += 1;
+      window.localStorage.setItem('losses', losses)
+
+      return {losses, result: "You loose"};
+      
+  
+    }
+  
+  
+  }
+
+  // to reset localstore
+  function setLocalStorage(){
+    window.localStorage.setItem('wins', 0)
+    window.localStorage.setItem('draws', 0)
+    window.localStorage.setItem('losses', 0)
+    wins.innerText = window.localStorage.getItem('wins')
 
   }
 
-
-
-  WinLogic(player, computer);
-
-
-}
